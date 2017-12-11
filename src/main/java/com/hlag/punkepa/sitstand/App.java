0d29427cf1199e8e6b52889c82aac8bcba82330c
punkepa <punkepa@hlag.com>
2017-12-11 13:19:12 +0100

    Initial Maven project

---
+++ src/main/java/com/hlag/punkepa/sitstand/App.java
@@ -0,0 +1,13 @@
+package com.hlag.punkepa.sitstand;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
